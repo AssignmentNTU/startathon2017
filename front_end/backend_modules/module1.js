b2ccd77a713b9fde006225ad445d90c539d718c3
Edward Sujono <sujonoe@garena.com>
2017-02-25 00:09:03 +0800

    init repository with express js

---
+++ front_end/backend_modules/module1.js
@@ -0,0 +1,9 @@
+
+
+
+module.exports = function(test, test1){
+    this.test = 1;
+    this.test1 = 2;
+
+
+};
---
+++ front_end/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var body_parser = require('body-parser');
+
+
+
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.json());
+
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', router);
+app.listen(port);
+console.log('Magic happens on port ' + port);
