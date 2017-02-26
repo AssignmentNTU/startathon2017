b8f085e6c124585f47fbc0001f46d7c1aaef11d0
mavericks9 <susantojason9@gmail.com>
2017-02-26 09:51:39 +0800

    jason feature

--- front_end/applikasi/src/pages/about/about.ts
+++ front_end/applikasi/src/pages/about/about.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import {PostsService} from '../services/posts.service';
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'about.html'
+  templateUrl: 'about.html',
+  providers: [PostsService]
 })
 export class AboutPage {
+  posts = []
+  public images = ["http://www.wildorganics.co.za/content/images/thumbs/0000210_butter-unsalted_100.jpeg", "http://www.humuch.com/item_images/a27613d5dab2c2f14c59a9e77911555bth.jpeg", "http://highermindsheadshop.co.uk/image/cache/data/MAIN/Cheese%20Strains-100x100.jpg"]
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private postService: PostsService) {
+    this.postService.getPosts("http://128.199.118.141:8008/api/list_item").subscribe(posts => {
+      this.posts = posts['data']
+      console.log(posts)
+    })
   }
 
 }
